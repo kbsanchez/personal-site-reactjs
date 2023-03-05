e9e4a13002d965844049119568777e5cbe055b4e
Keylin S <66031870+kbsanchez@users.noreply.github.com>
2023-03-05 13:39:33 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Route, Routes } from "react-router-dom"
 import NavBar from './components/NavBar/NavBar'
 import Home from './components/Home/Home'
-import ContactMe from './components/ContactMe'
-import Projects from './components/Projects'
-import Resume from './components/Resume'
+import ContactMe from './components/ContactMe/ContactMe'
+import Projects from './components/Projects/Projects'
+import Resume from './components/Resume/Resume'
 import Interests from './components/Interests'
 
 export default function App() {
