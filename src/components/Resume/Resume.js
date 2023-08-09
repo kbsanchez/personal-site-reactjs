aa41dbfacfd6ffa724a8fdcb8b00d4ec7c48c598
Keylin S <66031870+kbsanchez@users.noreply.github.com>
2023-08-08 20:25:40 -0400

    Updated contact page, added images for interests, updated readme

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -4,7 +4,7 @@ import ResumeContent from './ResumeContent'
 export default function Resume() {
 
     return (
-        <div className='flex flex-col justify-between min-h-[1000px] pt-10 h-screen bg-red-bean bg-opacity-95'>
+        <div className='flex flex-col justify-between pt-10 h-screen bg-red-bean bg-opacity-95'>
             <div className='resume-container mx-10 mt-[50px] mb-6'>
                 <div className='resume-parent min-h-full overflow-y-scroll'>
                     <ResumeContent />
